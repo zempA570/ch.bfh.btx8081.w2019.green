5848725f3544dbc4392117f26cb9c0b2c0af7309
kjasy <kjasy@DESKTOP-D9DBPBR.home>
2019-11-25 17:47:34 +0100

    important-notes

--- frontend/src/important-notes.js
+++ frontend/src/important-notes.js
@@ -1,6 +1,8 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 class ImportantNotes extends PolymerElement {
 
@@ -13,17 +15,22 @@ class ImportantNotes extends PolymerElement {
                 }
             </style>
 <vaadin-vertical-layout style="width: 100%; height: 100%; align-items: flex-start;">
- <label>Important Notes </label>
+ <label id="label">Important Notes </label>
  <vaadin-vertical-layout>
-  <vaadin-horizontal-layout style="flex-grow: 10; flex-shrink: 0; align-self: flex-start;">
+  <vaadin-horizontal-layout style="flex-grow: 10; flex-shrink: 0; align-self: flex-start;" id="vaadinHorizontalLayout">
    <label id="addDate">21.10.19</label>
    <label id="username" style="flex-grow: 0; flex-shrink: 0; align-self: flex-end;">Username </label>
-  </vaadin-horizontal-layout>Hat die Medikamente nicht eingenommen
-  <vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+  <label>Medikamente vergessen</label>Hat die Medikamente nicht eingenommen 
+  <vaadin-horizontal-layout id="vaadinHorizontalLayout1" content="Hat die Medikamente nicht eingenommen">
    <label style="align-start">18.10.19</label>
    <label id="username" style="align-end">Username </label>
-  </vaadin-horizontal-layout>Arzttermin vom 18.10.19: generelle Verschlechterung des Zustands wurde festgestellt
+  </vaadin-horizontal-layout>
+  <label>Arzttermin vom 18.10.19</label>generelle Verschlechterung des Zustands wurde festgestellt
  </vaadin-vertical-layout>
+ <vaadin-button id="vaadinButton" style="align-self: center;">
+  <iron-icon icon="lumo:plus" slot="prefix" id="ironIcon"></iron-icon>Add
+ </vaadin-button>
 </vaadin-vertical-layout>
 `;
     }
