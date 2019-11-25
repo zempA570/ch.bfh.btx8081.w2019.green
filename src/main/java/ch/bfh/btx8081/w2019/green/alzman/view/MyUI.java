c4158f44efccb5a412dfcd646be878514c4f833f
Adrian <adrian.zemp@students.bfh.ch>
2019-11-25 01:05:09 +0100

    Updated project structure according to MVP pattern

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/MyUI.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/MyUI.java
@@ -1,4 +1,4 @@
-package ch.bfh.btx8081.w2019.green.alzman;
+package ch.bfh.btx8081.w2019.green.alzman.view;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
