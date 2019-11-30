13465705131e969658d4ca7ddf284cb15d024d3c
Adrian <adrian.zemp@students.bfh.ch>
2019-11-30 16:00:33 +0100

    Added missing READMEs, project runs

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/MyUI.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/MyUI.java
@@ -1,5 +1,7 @@
 package ch.bfh.btx8081.w2019.green.alzman.view;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
@@ -13,17 +15,8 @@ public class MyUI extends VerticalLayout {
 
 	public MyUI() {
 
-		add(new DashboardGui());
-		add(new CalendarGui());
-		add(new InfoboxStart());
-		add(new RegisterPerson());
-		add(new ImportantNotes());
-		add(new CreateTodo());
-		add(new LoginUi());
-		add(new RegisterUser());
-		add(new TodoList());
-//		missing todo-list-overview.js
-//		add(new TodolistOverview());
+		Button button = new Button("Click me", event -> Notification.show("Clicked!"));
+		add(button);
 
 	}
 
