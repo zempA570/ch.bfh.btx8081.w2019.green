b9a8e0f9695e2b1612b74838641efb3b2e1a194a
Mootaas <mootaas.abubaker@students.bfh.ch>
2019-12-06 14:20:36 +0100

    Created Diary Class

---
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/Diary.java
@@ -0,0 +1,41 @@
+package ch.bfh.btx8081.w2019.green.alzman.view;
+
+import java.time.LocalDate;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.listbox.ListBox;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The user management view will be used to add and remove users of the app
+ */
+@Route("Diary")
+@CssImport(value = "./styles/shared-styles.css", include = "common-styles")
+public class Diary extends TemplateView {
+
+	public Diary() {
+
+		//Creating a new Textarea
+		TextArea textArea = new TextArea("");
+		textArea.setPlaceholder("");
+		
+		// Add a new Diary
+		Button button = new Button("Add new Diary", new Icon(VaadinIcon.PLUS));
+		
+		//Creating a new Vertical Layout
+		VerticalLayout verticalLayout = new VerticalLayout(button, textArea);
+		
+		//Adding components to content space
+		super.addContent(verticalLayout);
+
+		
+	}
+}
