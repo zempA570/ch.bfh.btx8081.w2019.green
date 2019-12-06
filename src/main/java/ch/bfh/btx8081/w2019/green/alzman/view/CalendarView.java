3693397fd8d3336f29fc0102ab41c59f74238278
Thevian <sinnt1@bfh.ch>
2019-12-06 13:45:55 +0100

    Build a calendar, two buttons (add and delete)

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/CalendarView.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/CalendarView.java
@@ -1,11 +1,15 @@
 package ch.bfh.btx8081.w2019.green.alzman.view;
 
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import org.vaadin.stefan.fullcalendar.Entry;
+import org.vaadin.stefan.fullcalendar.FullCalendar;
 import org.vaadin.stefan.fullcalendar.FullCalendarBuilder;
 import org.vaadin.stefan.fullcalendar.Resource;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.charts.model.Label;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -21,13 +25,19 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 
+/**
+ * 
+ * @author Thevian
+ * 
+ * This is a Calendargui
+ *
+ */
+
 @Route("Calendar")
 @CssImport(value = "./styles/shared-styles.css", include = "common-styles")
-public class CalendarView<FullCalendar> extends TemplateView {
+public class CalendarView extends TemplateView {
 	
-	private Label month;
 
-	private Calendar cal;
 
 
 	public CalendarView() {
@@ -35,16 +45,45 @@ public class CalendarView<FullCalendar> extends TemplateView {
 		// Change title in header
 		super.setHeaderTitle("Calendar");
 		ProgressBar progressBar = new ProgressBar();
-		progressBar.setIndeterminate(true);
 		
+		// add a progressbar
+		
+		progressBar.setIndeterminate(true);
 		super.add(progressBar);
 		
+		//Create and build a calendar
+		FullCalendar calendar = FullCalendarBuilder.create().build();
+		
+		super.addContent(calendar);
+		
+		// Create a initial sample entry
+		Entry entry = new Entry();
+		entry.setTitle("Some event");
+		entry.setStart(LocalDate.now().withDayOfMonth(3).atTime(10, 0), calendar.getTimezone());
+		entry.setEnd(entry.getStart().plusHours(2), calendar.getTimezone());
+		entry.setColor("#ff3333");
+
+		calendar.addEntry(entry);
+		
+		calendar.addTimeslotsSelectedListener((event) -> {
+		    entry.setStart(calendar.getTimezone().convertToUTC(event.getStartDateTime()));
+		    entry.setEnd(calendar.getTimezone().convertToUTC(event.getEndDateTime()));
+		    entry.setAllDay(event.isAllDay());
+
+		    entry.setColor("dodgerblue");
+
+		    // ... show and editor
+		});
+		
+		
+
+		//Create a horizontal Layout with two button. The button "calAdd" is for add an appointment. 
+		// The Button "calDel" ist for delete an entry.
 		
 		HorizontalLayout horiLay = new HorizontalLayout();
-		Image image = new Image("https://previews.123rf.com/images/zimmytws/zimmytws1201/zimmytws120100016/11996238-closeup-of-a-calendar-with-coming-soon-text-in-red.jpg", "In progress");
 		Button calAdd = new Button("Eintrag hinzufügen", event -> Notification.show("Funktion noch nicht verfügbar"));
 		Button calDel = new Button("Eintrag löschen", event -> Notification.show("Funktion noch nicht verfügbar"));
-		horiLay.add(calAdd, calDel, image);
+		horiLay.add(calAdd, calDel);
 		
 		super.add(horiLay);
 		super.add();
