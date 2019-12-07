71aeb8bb0d0cbb5422357b52c4abf7de6cad244e
Adrian <adrian.zemp@students.bfh.ch>
2019-12-07 15:37:24 +0100

    Added comments

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/DashboardView.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/DashboardView.java
@@ -14,7 +14,6 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
-
 @Route("")
 
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
@@ -30,100 +29,87 @@ public class DashboardView extends TemplateView {
 
 		Button btnOpenUserManagement = new Button("User Management", new Icon(VaadinIcon.USERS));
 		btnOpenUserManagement.addClickListener(e -> UI.getCurrent().navigate(UserManagementView.class));
-		
-		//Creating Elements
-		
-		
-		
+
+		// Creating Elements
+
 		/**
-		 * If we opt for multiple users we can add a colleciton
-		 * to the combobox below like in the example:
+		 * If we opt for multiple users we can add a colleciton to the combobox below
+		 * like in the example:
 		 * 
-		 * EXAMPLE
-		 * ComboBox(String label,
-        Collection<T> items)
-
-		Creates a combo box with the defined label and populated with the items in the collection.
+		 * EXAMPLE ComboBox(String label, Collection<T> items)
+		 * 
+		 * Creates a combo box with the defined label and populated with the items in
+		 * the collection.
 		 * 
 		 * 
-		 * NOTE: if we do choose to work with multiple users, code must be changed
-		 * to work for objects and not strings
+		 * NOTE: if we do choose to work with multiple users, code must be changed to
+		 * work for objects and not strings
 		 */
-	ComboBox <String> chooseUserBox = new ComboBox<>("Users");
-	chooseUserBox.setItems("TEST", "DUMMY", "BOT");
-	
-	chooseUserBox.addValueChangeListener(event -> {
-		Button message = null;
-		if (event.getSource().isEmpty()) {
-			message.setText("The current User is TEST");
-			
-		} else {
-			message.setText("The current User is" + event.getValue());
-		}
-	});
-	
-	Icon userIcon = new Icon(VaadinIcon.USERS);
-	
-	
-	/**
-	 * creating the Icons for the Dashboard
-	 */
-	Icon calendarIcon = new Icon(VaadinIcon.CALENDAR);
-	
-	Icon statusIcon = new Icon (VaadinIcon.BULLETS);
-	
-	Icon importantNotesIcon = new Icon(VaadinIcon.NOTEBOOK);
-	
-	Icon diaryIcon = new Icon(VaadinIcon.EDIT);
-	
-	Icon todoIcon = new Icon(VaadinIcon.TASKS);
-	
-	Icon infoBoxIcon = new Icon(VaadinIcon.INFO_CIRCLE);
-	
-	// change Icon Size and Shape
-	calendarIcon.setSize("80px");
-	calendarIcon.setColor("burgundy");
-	statusIcon.setSize("80px");
-	importantNotesIcon.setSize("80px");
-	diaryIcon.setSize("80px");
-	todoIcon.setSize("80px");
-	infoBoxIcon.setSize("80px");
-	
-	// Test Clicks implementation
-	Div message = new Div();
-	calendarIcon.getStyle().set("cursor", "pointer");
-	calendarIcon.addClickListener(event ->
-	message.setText("Calendar has been selected"));
-	
-	//creating labels to label the icons
-	Label calendarText = new Label("Calendar");
-	Label statusText = new Label("Status");
-	Label importantNotesText = new Label("Key-Notes");
-	Label diaryText = new Label("Diary");
-	Label todoText = new Label("To-Do-List");
-	Label infoBoxText = new Label("Infobox");
-	
-	
-	
-	
-	
-	
-	
-	//configuring the layout
-	VerticalLayout iconViewLeft = new VerticalLayout(calendarText, calendarIcon, statusText, statusIcon, importantNotesText,importantNotesIcon);
-	VerticalLayout iconViewRight = new VerticalLayout(diaryText, diaryIcon, todoText,todoIcon,infoBoxText,infoBoxIcon);
-	HorizontalLayout userBar = new HorizontalLayout(chooseUserBox);
-	HorizontalLayout iconViewCentral = new HorizontalLayout(iconViewLeft,iconViewRight);
+		ComboBox<String> chooseUserBox = new ComboBox<>("Users");
+		chooseUserBox.setItems("TEST", "DUMMY", "BOT");
 
-	//adding Stuff to the Objects
+		chooseUserBox.addValueChangeListener(event -> {
+			Button message = null;
+			if (event.getSource().isEmpty()) {
+				message.setText("The current User is TEST");
+
+			} else {
+				message.setText("The current User is" + event.getValue());
+			}
+		});
+
+		Icon userIcon = new Icon(VaadinIcon.USERS);
+
+		/**
+		 * creating the Icons for the Dashboard
+		 */
+		Icon calendarIcon = new Icon(VaadinIcon.CALENDAR);
+
+		Icon statusIcon = new Icon(VaadinIcon.BULLETS);
+
+		Icon importantNotesIcon = new Icon(VaadinIcon.NOTEBOOK);
+
+		Icon diaryIcon = new Icon(VaadinIcon.EDIT);
+
+		Icon todoIcon = new Icon(VaadinIcon.TASKS);
+
+		Icon infoBoxIcon = new Icon(VaadinIcon.INFO_CIRCLE);
+
+		// change Icon Size and Shape
+		calendarIcon.setSize("80px");
+		calendarIcon.setColor("burgundy");
+		statusIcon.setSize("80px");
+		importantNotesIcon.setSize("80px");
+		diaryIcon.setSize("80px");
+		todoIcon.setSize("80px");
+		infoBoxIcon.setSize("80px");
+
+		// Test Clicks implementation
+		Div message = new Div();
+		calendarIcon.getStyle().set("cursor", "pointer");
+		calendarIcon.addClickListener(event -> message.setText("Calendar has been selected"));
+
+		// creating labels to label the icons
+		Label calendarText = new Label("Calendar");
+		Label statusText = new Label("Status");
+		Label importantNotesText = new Label("Key-Notes");
+		Label diaryText = new Label("Diary");
+		Label todoText = new Label("To-Do-List");
+		Label infoBoxText = new Label("Infobox");
+
+		// configuring the layout
+		VerticalLayout iconViewLeft = new VerticalLayout(calendarText, calendarIcon, statusText, statusIcon,
+				importantNotesText, importantNotesIcon);
+		VerticalLayout iconViewRight = new VerticalLayout(diaryText, diaryIcon, todoText, todoIcon, infoBoxText,
+				infoBoxIcon);
+		HorizontalLayout userBar = new HorizontalLayout(chooseUserBox);
+		HorizontalLayout iconViewCentral = new HorizontalLayout(iconViewLeft, iconViewRight);
+
+		// adding Stuff to the Objects
 		super.addContent(btnOpenUserManagement);
 		super.addContent(userBar);
 		super.addContent(iconViewCentral);
-		
-		
-	}
-	
-	
+
 	}
 
-
+}
