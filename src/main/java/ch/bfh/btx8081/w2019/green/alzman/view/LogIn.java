15b44a3ae17eb7562e582c0f3e25a73f37dbd377
Jasmitha <devaj1@bfh.ch>
2019-12-10 21:22:38 +0100

    route is filled out

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/LogIn.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/LogIn.java
@@ -16,7 +16,7 @@ import com.vaadin.flow.router.Route;
 /**
  * The user management view will be used to add and remove users of the app
  */
-@Route("")
+@Route("Login")
 @CssImport(value = "./styles/shared-styles.css", include = "common-styles")
 public class LogIn extends TemplateView {
 
