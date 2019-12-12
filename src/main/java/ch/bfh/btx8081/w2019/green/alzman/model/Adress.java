02a145255f6e2f5ae90e86094326fd17b2bb112c
uthag1 <47814028+uthag1@users.noreply.github.com>
2019-12-13 00:04:28 +0100

    Test file

---
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/model/Adress.java
@@ -0,0 +1,16 @@
+package ch.bfh.btx8081.w2019.green.alzman.model;
+
+public class Adress {
+
+	String postcode;
+	String city;
+	String phonenummber;
+
+	public Adress(String postcode, String city, String phonenummber) {
+		super();
+		this.postcode = postcode;
+		this.city = city;
+		this.phonenummber = phonenummber;
+	}
+
+}
