65a033a827587100e0fc5f359d1e395f9a760c15
uthag1 <47814028+uthag1@users.noreply.github.com>
2019-12-12 21:44:34 +0100

    Addmerson molde class is made

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/model/AddPerson.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/model/AddPerson.java
@@ -1,5 +1,76 @@
 package ch.bfh.btx8081.w2019.green.alzman.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+@Entity
+@Table(name = "ImportantPerson")
 public class AddPerson {
+	
+	@TableGenerator(name = "ImportantPersonIDGenerator", allocationSize = 1, initialValue = 1)
+	@Id
+	@GeneratedValue(generator = "ImportantPersonIDGenerator")
+	int id;
+	
+	String firstname;
+	String lastname;
+	String adress;
+	String adressNr;
+	String postcode;
+	String city;
+	String phonenummber;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getAdressNr() {
+		return adressNr;
+	}
+	public void setAdressNr(String adressNr) {
+		this.adressNr = adressNr;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPhonenummber() {
+		return phonenummber;
+	}
+	public void setPhonenummber(String phonenummber) {
+		this.phonenummber = phonenummber;
+	}
+	
+	
 
 }
