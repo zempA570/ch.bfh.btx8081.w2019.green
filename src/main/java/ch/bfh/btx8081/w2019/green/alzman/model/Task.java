2c1bc8ece5d8248090fe0b1a30907838f73c0c6e
Jasmitha <devaj1@bfh.ch>
2019-12-13 10:30:18 +0100

    opened Task-model class

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/model/Task.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/model/Task.java
@@ -1,5 +1,12 @@
 package ch.bfh.btx8081.w2019.green.alzman.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Tasks")
+
 public class Task {
 
+	//@TableGenerator(name = )
 }
