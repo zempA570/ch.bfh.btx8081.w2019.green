d40e38dc1176598774e2635207fe5c5c6d2ccc76
Thevian <sinnt1@bfh.ch>
2019-12-23 23:14:39 +0100

    Add and delete function for the Calendar is done.

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/presenter/CalendarPresenter.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/presenter/CalendarPresenter.java
@@ -27,36 +27,68 @@ import ch.bfh.btx8081.w2019.green.alzman.view.UserManagementView;
 public class CalendarPresenter {
 
 	private CalendarView Cview;
-	
+	private List<CalendarModel> entries;
+
 	// constructor for the presenter
 	public CalendarPresenter(CalendarView calendarView) {
 		this.Cview = calendarView;
 
+		insertEntriesToCalendar();
 
 	}
 
-	
-
-
 	public void addAppointmentToDB(LocalDate LdatePick, LocalTime LtimePick, String author, String Entry) {
 
-		
 		Date datePick = Date.valueOf(LdatePick);
-		
+
 		Time time = Time.valueOf(LtimePick);
 		// create new user
-		
-		CalendarModel addAppointment = new CalendarModel(datePick,time,author,Entry);
+
+		CalendarModel addAppointment = new CalendarModel(datePick, time, author, Entry);
 
 		// DB stuff
+		DbService.init();
 		DbService.em.getTransaction().begin();
 		DbService.em.persist(addAppointment);
 		DbService.em.getTransaction().commit();
 
-	
 	}
-	
-	
 
-	
+	public void deleteAppointment(String id) {
+
+		CalendarModel entryToDelete = null;
+
+		for (CalendarModel entries : entries) {
+
+			if (Objects.equals(entries.getId(), id))
+
+				System.out.println(entries.getId());
+			System.out.println(id);
+
+			{
+
+				entryToDelete = entries;
+			}
+
+		}
+		if (entryToDelete != null) {
+			DbService.em.getTransaction().begin();
+			DbService.em.remove(entryToDelete);
+			DbService.em.getTransaction().commit();
+
+		}
+
+	}
+
+	public void insertEntriesToCalendar() {
+
+		Query query = DbService.em.createNativeQuery("SELECT * FROM CALENDARMODEL", CalendarModel.class);
+
+		entries = query.getResultList();
+
+		for (CalendarModel entries : entries) {
+			Cview.addEntryToCalendar(entries);
+		}
+
+	}
 }
--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/CalendarView.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/view/CalendarView.java
@@ -1,6 +1,9 @@
 package ch.bfh.btx8081.w2019.green.alzman.view;
 
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -13,6 +16,7 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.charts.model.Label;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.Icon;
@@ -23,7 +27,12 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.progressbar.ProgressBar;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.router.Route;
+import com.vaadin.shared.ui.datefield.LocalDateTimeFieldState;
+
+import ch.bfh.btx8081.w2019.green.alzman.model.CalendarModel;
+import ch.bfh.btx8081.w2019.green.alzman.presenter.CalendarPresenter;
 
 
 /**
@@ -39,7 +48,8 @@ import com.vaadin.flow.router.Route;
 public class CalendarView extends TemplateView {
 	
 
-
+public CalendarPresenter presenter;
+public FullCalendar calendar;
 
 	public CalendarView() {
 
@@ -49,11 +59,11 @@ public class CalendarView extends TemplateView {
 		
 		// add a progressbar
 		
-		progressBar.setIndeterminate(true);
-		super.add(progressBar);
+		//progressBar.setIndeterminate(true);
+		//super.add(progressBar);
 		
 		//Create and build a calendar
-		FullCalendar calendar = FullCalendarBuilder.create().build();
+		calendar = FullCalendarBuilder.create().build();
 		
 		super.addContent(calendar);
 		
@@ -63,6 +73,7 @@ public class CalendarView extends TemplateView {
 		entry.setStart(LocalDate.now().withDayOfMonth(3).atTime(10, 0), calendar.getTimezone());
 		entry.setEnd(entry.getStart().plusHours(2), calendar.getTimezone());
 		entry.setColor("#ff3333");
+		entry.setEditable(false);
 
 		calendar.addEntry(entry);
 		
@@ -83,18 +94,105 @@ public class CalendarView extends TemplateView {
 		
 		HorizontalLayout horiLay = new HorizontalLayout();
 		Button calAdd = new Button("Eintrag hinzufügen");
-		calAdd.addClickListener(e -> UI.getCurrent().navigate(AddAppointment.class));
-		Button calDel = new Button("Eintrag löschen");
-		calDel.addClickListener(e -> UI.getCurrent().navigate(DeleteAppointment.class));
-
-		horiLay.add(calAdd, calDel);
 		
+		Button calDel = new Button("Eintrag löschen");
+		
+
+		
+		DatePicker datePick = new DatePicker();
+		datePick.setValue(LocalDate.now());
+		datePick.setLabel("Datum");
+		
+
+		// Display an icon which can be clicked to clear the value:
+		datePick.setClearButtonVisible(true);
+		
+		TimePicker time = new TimePicker();
+		time.setValue(LocalTime.now());
+		time.setLabel("Zeit");
+
+		// Display an icon which can be clicked to clear the value:
+		time.setClearButtonVisible(true);
+		
+		
+
+		TextField author = new TextField();
+		author.setLabel("Geschrieben von");
+		
+		TextField  entry1 = new TextField();
+		entry1.setLabel("Eintrag");
+
+		TextField  deleteId = new TextField();
+		deleteId.setLabel("ID for delete an entry");
+	
+
+		// Creating horizontal layout
+		HorizontalLayout dateTimeHLayout = new HorizontalLayout();
+		
+		dateTimeHLayout.add(datePick, time);
+		
+		// Creating horizontal layout
+		HorizontalLayout textHLayount = new HorizontalLayout();
+		
+		textHLayount.add(author, entry1);
+
+		horiLay.add(calAdd, calDel, deleteId);
+
+
+		
+		
+
+		// Adding components to content space
+	
 		super.add(horiLay);
-		super.add();
+		
+		super.add(dateTimeHLayout);
+		super.add(textHLayount);
+		
+		presenter = new CalendarPresenter(this);
+		
+		
+		
+		calAdd.addClickListener(e -> presenter.addAppointmentToDB(datePick.getValue(), time.getValue(),author.getValue(), entry1.getValue()));
+		calAdd.addClickListener(e -> UI.getCurrent().getPage().reload());
+
+
+		calDel.addClickListener(e -> presenter.deleteAppointment(deleteId.getValue()));
+		calDel.addClickListener(e -> UI.getCurrent().getPage().reload());
+
+		
+	
 		
 
 		
 
 	}
+	public void addEntryToCalendar(CalendarModel cModel) {
+		
+		// Create a initial sample entry
+		Entry entry = new Entry();
+		entry.setTitle(cModel.getEntry() + " "+cModel.getId());
+		entry.setDescription(cModel.getAuthor());
+		
+		
+		LocalDateTime actDateTime =  LocalDateTime.of(cModel.getDate().toLocalDate(), cModel.getTime().toLocalTime());
+		
+		
+		entry.setStart(actDateTime);
+		//entry.setEnd(entry.getStart().plusHours(2), calendar.getTimezone());
+		
+		entry.setColor("#ff3333");
+		entry.setEditable(true);
+
+		calendar.addEntry(entry);
+		
+		
+		
+		
+		
+	}
+	
+
+	
 
 }
