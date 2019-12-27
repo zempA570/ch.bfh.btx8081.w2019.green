22ef3698b3250a5f06718dc1ac4e0ffce163e0d5
Adrian <adrian.zemp@students.bfh.ch>
2019-12-27 11:20:12 +0100

    Merge mistake on my part; this is the correct version

--- src/main/java/ch/bfh/btx8081/w2019/green/alzman/model/AddDiaryModel.java
+++ src/main/java/ch/bfh/btx8081/w2019/green/alzman/model/AddDiaryModel.java
@@ -1,6 +1,8 @@
 package ch.bfh.btx8081.w2019.green.alzman.model;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 import com.vaadin.flow.component.datepicker.DatePicker;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,9 +19,7 @@ public class AddDiaryModel {
 	@GeneratedValue
 	int id;
 
-	DatePicker datee;
-	SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-	String date = formatter.format(datee);
+	LocalDate date;
 	String author;
 	String Entry;
 
@@ -31,17 +31,17 @@ public class AddDiaryModel {
 
 	}
 
-	public AddDiaryModel(String date, String author, String entry) {
+	public AddDiaryModel(LocalDate date, String author, String entry) {
 		this.date = date;
 		this.author = author;
 		this.Entry = entry;
 	}
 
-	public String getDate() {
+	public LocalDate getDate() {
 		return date;
 	}
 
-	public void setDate(String date) {
+	public void setDate(LocalDate date) {
 		this.date = date;
 	}
 
@@ -59,6 +59,10 @@ public class AddDiaryModel {
 
 	public void setEntry(String entry) {
 		Entry = entry;
+	}
+
+	public String getFullDiary() {
+		return author + " " + date + " " + Entry;
 	}
 
 	public int getId() {
